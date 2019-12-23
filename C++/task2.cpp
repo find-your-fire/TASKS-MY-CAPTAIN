6ac029ee11c6d39f87e5f7c7b430aa46bf44ba26
find-your-fire <55709811+find-your-fire@users.noreply.github.com>
2019-12-23 22:43:09 +0530

    task 2 updated

---
+++ C++/task2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"Enter the number\n";
+	cin>>n;
+	if(n%2==0)
+	{
+		cout<<n<<" is Even\n";
+	}
+	else
+	{	
+		cout<<n<<" is Odd\n";
+	}	
+	return 0;
+}
